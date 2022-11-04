48ba5118dbcd2d8042e9138ef8c36d4a0051b287
unknown <Savioanjos2020@gmail.com>
2022-11-04 17:40:44 -0300

    Enviando alterações

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -8,27 +8,42 @@ export default function Home() {
 
   return (
 
-    <div>
+    <div className='max-w-[1124px] h-screen mx-auto grid grid-cols-2 gap-28 items-center'>
       <main>
           <Image src={logoImg} alt="NLW copa"/> 
 
-          <h1>Crie seu próprio bolão da copa e compartilhe entre amigos!</h1>
-      </main>
+          <h1 className='mt-14 text-white text-5xl font-bold leading-tight'>
+            Crie seu próprio bolão da copa e compartilhe entre amigos!
+          </h1>
       
-      <div>
+      
+      <div className='mt-10 flex items-center gap-2'>
       <Image src={usersAvatarExempleImg} alt="" />
 
-      <strong>
-        <span>+12.592</span>pessoas já estão usando
+      <strong className='text-gray-100 text-xl'>
+        <span className='text-ignite-500'>+12.592</span>pessoas já estão usando
       </strong>
       </div>
 
-      <form>
-        <input type="text" required placeholder='Qual nome do seu bolão?'/>
-        <button type='submit'>Criar meu Bolão</button>
+      <form className='mt-10 flex gap-2'>
+        <input
+        className='flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm'
+         type="text"
+         required
+         placeholder='Qual nome do seu bolão?'
+         />
+
+        <button 
+        type='submit'
+        className='bg-yellow-500 px-6 py-4 rounded text-gray-900 font-bold text-sm uppercase hover:bg-yellow-700'
+        >
+          Criar meu Bolão
+          </button>
       </form>
 
-      <p>Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas 🚀</p>
+      <p className='mt-4 text-sm text-gray-300 leading-relaxed'>
+        Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas 🚀
+      </p>
 
       <div>
         <div>
@@ -47,6 +62,7 @@ export default function Home() {
             </div>
         </div>
       </div>
+      </main> 
      
 
       <Image 
@@ -54,6 +70,8 @@ export default function Home() {
       alt="Dois celulares exibindo uma prévia da aplicação móvel do NLW copa."
       quality={100}
       />
+
+
     </div>
     
   )
